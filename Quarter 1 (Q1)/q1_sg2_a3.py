afcca6b0e6aedd5e3dc47b0db5d0a4f2bbd79f63
Solus-Tenebris(#3-Samat) <jdarellano2030@clc.pshs.edu.ph>
2026-08-16 21:41:44 +0800

    Update q1_sg2_a3.py

--- Quarter 1 (Q1)/q1_sg2_a3.py
+++ Quarter 1 (Q1)/q1_sg2_a3.py
@@ -1,1 +1,29 @@
+'''
+#3 Arellano, Joaquin Ethan D.
+9-Samat
+August 16, 2026
+'''
+zodiac_signs = ["Rat (鼠 / Shǔ)",
+"Ox (牛 / Niú)",
+"Tiger (虎 / Hǔ)", 
+"Rabbit (兔 / Tù)",
+"Dragon (龙 / Lóng)",
+"Snake (蛇 / Shé)",
+"Horse (马 / Mǎ)",
+"Goat (羊 / Yáng)",
+"Monkey (猴 / Hóu)",
+"Rooster (鸡 / Jī)",
+"Dog (狗 / Gǒu)",
+"Pig (猪 / Zhū)" ]
+
+age = int(input("Enter your birth year: "))
+baseline = 1900
+
+
+if age < 1900:
+    print ("Invalid Year, it should not be earlier than 1900.")
+else:
+    result = (age - baseline)%12
+    print (f"Your Chinese Zodiac Sign is : {zodiac_signs[result]}"))
+
 
